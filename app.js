93a0c329a887e9a62cb23996a86f554a54b89f09
dtechiegirl <79475136+dtechiegirl@users.noreply.github.com>
2022-01-05 19:35:14 +0100

    Add files via upload
    counter

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
-'use strict'
+//initial count
 
-const colors = [
-  "green",
-  "red",
-  "rgba(133,122,200)",
-  "#f15025",
-  "hotpink",
-  "aqua",
-  "grey",
-  "coral",
-  "tomato",
-];
+let count = 0;
+const value = document.querySelector("#value");
+let btns = document.querySelectorAll(".btn");
 
-const btn = document.getElementById("btn");
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count === 0;
+    }
 
-const color = document.querySelector(".color");
-
-btn.addEventListener("click", function () {
-  const randomNo = getRandomNo();
-  // console.log(randomNo)
-
-  document.body.style.backgroundColor = colors[randomNo];
-
-  color.textContent = colors[randomNo];
+    if (count > 0) {
+      value.style.color = "green";
+    } else if (count < 0) {
+      value.style.color = "red";
+    } else if (count === 0) {
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
 });
-
-function getRandomNo() {
-  return Math.trunc(Math.random() * colors.length);
-}
